3e544873a30b97d674e70345895b8501f3d81c45
francisco ximenes <fximenessilva@gmail.com>
2021-04-28 15:38:42 +0100

    add env variables

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-underscore-dangle */
 const express = require('express');
+const dotenv = require('dotenv');
 const products = require('./data/products');
+
+dotenv.config();
 
 const app = express();
 
@@ -14,4 +17,7 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log('Server running on port 5000'));
+const { PORT } = process.env;
+
+app.listen(PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
